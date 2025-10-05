84346fc663a98d883c7fbb598ffe49d4a135e799
Justin Hunt <bitwalkerjapan@gmail.com>
2025-10-05 17:35:59 +0900

    verson bump

--- version.php
+++ version.php
@@ -32,5 +32,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_englishcentral';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2015051100; // Moodle 2.9 (because we want to use AMD)
-$plugin->version   = 2025080146;
+$plugin->version   = 2025080148;
 $plugin->release   = '2025-10-05 (48)';
